cac0d460e6140b071debcd4f7efbc5ae3012773c
Niles <Validarkness@gmail.com>
2019-08-28 18:57:39 -0500

    Update Dumpster.d.ts

--- Dumpster.d.ts
+++ Dumpster.d.ts
@@ -1,13 +1,26 @@
-declare class Dumpster {
+/** A cleanup object into which items (Instances, Connections, etc) can be dumped and later burned.
+ * @author fractality
+*/
+interface Dumpster {
 	/**
 	 * Adds `item` to the dumpster. Will be cleaned up on `Dumpster.burn`.
+	 * @param item The function to call, instance to destroy, or connection to disconnect.
 	 */
-	public dump(item: Function | Instance | RBXScriptConnection): void;
+	dump(item: Function | Instance | RBXScriptConnection): void;
 
 	/**
-	 * Cleans all items
+	 * Adds `item` to the dumpster. Will be cleaned up on `Dumpster.burn` by the `burner` callback.
+	 * @param item The item to pass into the `burner` function.
+	 * @param burner The function to call on item to clean it up.
 	 */
-	public burn(): void;
+	dump<T>(item: T, burner: (item: T) => void): void;
+
+	/**
+	 * Cleans all items.
+	 */
+	burn(): void;
 }
+
+declare const Dumpster: new () => Dumpster;
 
 export = Dumpster;
